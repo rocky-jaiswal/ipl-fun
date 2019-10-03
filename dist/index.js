d4424d679191441ce0fbbe37aa2d642dc1877643
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-10-03 10:15:54 +0200

    minor fixes.

--- dist/index.js
+++ dist/index.js
@@ -11,39 +11,41 @@ aws_sdk_1.default.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
-const results = [];
-const Bucket = 'de.rockyj.ipl-data';
 const s3 = new aws_sdk_1.default.S3();
+const Bucket = 'de.rockyj.ipl-data';
+const results = [];
+let processed = 0;
+let totalNumberToProcess = 0;
+const workInThread = (rawData, resolve) => {
+    const worker = new worker_threads_1.Worker('./dist/worker.js', { workerData: rawData });
+    worker.once('message', message => {
+        results.push(message);
+        processed += 1;
+        if (processed === totalNumberToProcess) {
+            resolve(results);
+        }
+    });
+};
+const processS3Bucket = (s3Data, reject, resolve) => {
+    totalNumberToProcess = s3Data.length;
+    s3Data.forEach((content) => s3.getObject({ Bucket, Key: content.Key }, (err, yamlData) => processS3Object(err, yamlData, reject, resolve)));
+};
+const processS3Object = (err, yamlData, reject, resolve) => {
+    if (!err) {
+        workInThread(yamlData.Body.toString(), resolve);
+    }
+    else {
+        console.error(err);
+        reject();
+    }
+};
 new Promise((resolve, reject) => {
     s3.listObjects({ Bucket }, (err, data) => {
         if (err) {
-            console.error('Error', err);
+            console.error(err);
             reject();
         }
-        let count = 0;
-        const numberToProcess = data.Contents.length;
-        const work = (rawData) => {
-            const worker = new worker_threads_1.Worker('./dist/worker.js', { workerData: rawData });
-            worker.once('message', message => {
-                count += 1;
-                results.push(message);
-                if (count === numberToProcess) {
-                    resolve(results);
-                }
-            });
-        };
-        data.Contents.forEach(content => {
-            const Key = content.Key;
-            s3.getObject({ Bucket, Key }, (err, yamlData) => {
-                if (err) {
-                    console.error('Error', err);
-                    reject();
-                }
-                else {
-                    work(yamlData.Body.toString());
-                }
-            });
-        });
+        processS3Bucket(data.Contents, reject, resolve);
     });
 }).then((results) => {
     const { finalResult, mostRunsMatch } = aggregateResult_1.aggregateResult(results);
--- src/index.ts
+++ src/index.ts
@@ -13,38 +13,46 @@ const s3 = new AWS.S3();
 const Bucket = 'de.rockyj.ipl-data';
 const results: Array<any> = [];
 
+let processed = 0;
+let totalNumberToProcess = 0;
+
+const workInThread = (rawData: string, resolve: Function) => {
+  const worker = new Worker('./dist/worker.js', { workerData: rawData });
+  worker.once('message', message => {
+    results.push(message);
+    processed += 1;
+    if (processed === totalNumberToProcess) {
+      resolve(results);
+    }
+  });
+};
+
+const processS3Bucket = (s3Data: any, reject: Function, resolve: Function) => {
+  totalNumberToProcess = s3Data.length;
+  s3Data.forEach((content: any) =>
+    s3.getObject({ Bucket, Key: content!.Key! }, (err, yamlData) =>
+      processS3Object(err, yamlData, reject, resolve)
+    )
+  );
+};
+
+const processS3Object = (err: any, yamlData: any, reject: Function, resolve: Function) => {
+  if (!err) {
+    workInThread(yamlData.Body!.toString(), resolve);
+  } else {
+    console.error(err);
+    reject();
+  }
+};
+
 new Promise((resolve, reject) => {
   s3.listObjects({ Bucket }, (err, data) => {
     if (err) {
-      console.error('Error', err);
+      console.error(err);
       reject();
     }
 
-    let count = 0;
-    const numberToProcess = data.Contents!.length;
-
-    const work = (rawData: string) => {
-      const worker = new Worker('./dist/worker.js', { workerData: rawData });
-      worker.once('message', message => {
-        count += 1;
-        results.push(message);
-        if (count === numberToProcess) {
-          resolve(results);
-        }
-      });
-    };
-
-    data.Contents!.forEach(content => {
-      const Key = content!.Key!;
-      s3.getObject({ Bucket, Key }, (err, yamlData) => {
-        if (err) {
-          console.error('Error', err);
-          reject();
-        } else {
-          work(yamlData.Body!.toString());
-        }
-      });
-    });
+    processS3Bucket(data.Contents!, reject, resolve);
   });
 }).then((results: any) => {
   const { finalResult, mostRunsMatch } = aggregateResult(results);
